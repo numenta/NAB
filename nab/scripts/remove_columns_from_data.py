010c96934f5e2977a31a2e77b9cb57e80f7645a9
Jay Gokhale <jgokhale@jaygokhalesmbp.corp.numenta.com>
2014-08-18 13:57:38 -0700

    Formatted datasets that were jsut added

--- nab/scripts/remove_columns_from_data.py
+++ nab/scripts/remove_columns_from_data.py
@@ -9,7 +9,6 @@ depth = 3
 root = recur(os.path.dirname, os.path.realpath(__file__), depth)
 
 def main(args):
-
   if not args.absolutePaths:
     args.dataDir = os.path.join(root, args.dataDir)
     if args.destDir:
@@ -17,7 +16,8 @@ def main(args):
 
   corpus = Corpus(args.dataDir)
 
-  corpus.removeColumn('label', write=True, newRoot=args.destDir)
+  for name in args.columnNames:
+    corpus.removeColumn(name, write=True, newRoot=args.destDir)
 
 
 if __name__ == '__main__':
@@ -36,6 +36,11 @@ if __name__ == '__main__':
                       NAB root",
                       default=False,
                       action="store_true")
+
+  parser.add_argument("--columnNames",
+                      help="All the names of the columns which must be removed",
+                      nargs='+',
+                      type=str)
 
   args = parser.parse_args()
   main(args)
