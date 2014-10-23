87a7295e5d39ded492e98e337f688efb595d2600
Subutai Ahmad <sahmad@numenta.com>
2014-10-23 11:51:40 -0700

    Added simple random detector
    Makes testing go faster.
    Provides a simplistic baseline to ensure scoring and the rest of the system is working above a non-trivial level.
    Renamed ground truth files again.
    Reformatted ground truth file.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,6 @@
-echo 
+#!/bin/bash
+
+echo
 echo "========================================"
 echo "Running integration tests"
 
@@ -6,6 +8,8 @@ python tests/integration/false_negative_test.py
 python tests/integration/false_positive_test.py 
 python tests/integration/true_positive_test.py
 
+# This should really be in a python script and send output to a temporary
+# file outside the repository.
 echo 
 echo "========================================"
 echo "Running end to end tests"
@@ -15,4 +19,9 @@ time python run.py --numCPUs 3 --dataDir tests/test_data \
                    --labelFile tests/test_labels/user_label.json \
                    --thresholdsFile tests/test_config/thresholds.json \
                    --resultsDir tests/test_results \
-                   --skipConfirmation
+                   --skipConfirmation \
+                   --detectors random
+
+# Add end to end test with just does optimize
+
+# Add end to end test with just does scoring
