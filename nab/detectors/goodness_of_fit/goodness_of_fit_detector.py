c2b63293000bbcb2d546bdd416b727f9a3cb597c
zuhaagha <zua2@pitt.edu>
2016-07-13 09:43:07 -0700

    review feedback

--- nab/detectors/goodness_of_fit/goodness_of_fit_detector.py
+++ nab/detectors/goodness_of_fit/goodness_of_fit_detector.py
@@ -1,5 +1,26 @@
-import numpy
+# ----------------------------------------------------------------------
+# Copyright (C) 2016, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
 import math
+import numpy
+
 from scipy import stats
 
 from nab.detectors.base import AnomalyDetector
@@ -9,7 +30,7 @@ from nab.detectors.base import AnomalyDetector
 class GoodnessOfFitDetector(AnomalyDetector):
 
   """ This detector is an implementation of online anomaly detection using
-  Relative Entropy statistic with multiple hypothesis as described in
+  Relative Entropy statistic with multiple hypotheses as described in
   Figure 1 of Wang, Chengwei, et al. "Statistical Techniques for Online
   Anomaly Detection in Data Centers", Proceedings of the 8th ACM
   international conference on Autonomic computing. ACM, 2011.
@@ -17,8 +38,8 @@ class GoodnessOfFitDetector(AnomalyDetector):
   The algorithm is based on a hypothesis testing approach that compares
   observed data against multiple null hypotheses, representing frequencies of
   quantized data over a window. If the observed data is unseen and does not
-  agree with any existing hypothesis, it is declared anomolous and a new
-  hypothesis is created. Otherwise, it is declared non-anomolous, provided that
+  agree with any existing hypothesis, it is declared anomalous and a new
+  hypothesis is created. Otherwise, it is declared non-anomalous, provided that
   the accepted hypothesis occurs frequently enough. Decision to accept/reject a
   null hypothesis is based on relative entropy compared against a threshold
   of acceptable false negative probability determined by the chi-squared
@@ -27,6 +48,9 @@ class GoodnessOfFitDetector(AnomalyDetector):
   """
 
   def __init__(self, *args, **kwargs):
+    """ Variable names are kept consistent with algorithm's pseudo code in
+    the paper."""
+
     super(GoodnessOfFitDetector, self).__init__(*args, **kwargs)
 
     # Timeseries of the metric on which anomaly needs to be detected
@@ -49,8 +73,8 @@ class GoodnessOfFitDetector(AnomalyDetector):
     # Tracks the current number of null hypothesis
     self.m = 0
 
-    # Step size in timeseries quantization
-    self.stepSize = (self.inputMax - self.inputMin)/self.N_bins
+    # Step size in time series quantization
+    self.stepSize = (self.inputMax - self.inputMin) / self.N_bins
 
     # List of lists where P[i] indicates the empirical frequency of the ith
     # hypothesis.
@@ -72,8 +96,7 @@ class GoodnessOfFitDetector(AnomalyDetector):
     """
 
     anomalyScore = 0.0
-    inputValue = inputData["value"]
-    self.util.append(inputValue)
+    self.util.append(inputData["value"])
 
     #  This check is for files where self.inputMin == self.input max i.e
     #  all data points are identical and stepSize is 0 e.g
@@ -93,13 +116,14 @@ class GoodnessOfFitDetector(AnomalyDetector):
         B_current = [math.ceil((c - self.inputMin) / self.stepSize) for c in
                      util_current]
 
-        # Create a histogram of empirial frequencies for the current window
+        # Create a histogram of empirical frequencies for the current window
         # using B_current
         P_hat = numpy.histogram(B_current,
                                 bins=self.N_bins,
                                 range=(0,self.N_bins),
                                 density=True)[0]
 
+        # This is for the first null hypothesis
         if self.m == 0:
           self.P.append(P_hat)
           self.c.append(1)
@@ -140,15 +164,17 @@ class GoodnessOfFitDetector(AnomalyDetector):
     freedom.
 
     The function returns the index of hypothesis that agrees with minimum
-    relative entropy.If all hypothesis disagree,the function returns -1.
+    relative entropy. If all hypotheses disagree, the function returns -1.
 
-    @param P_hat: empirical frequency of the current window
-    @return: index of the hypothesis with the minimum test statistic
+    @param P_hat    (list)  Empirical frequencies of the current window.
+
+    @return index   (int)   Index of the hypothesis with the minimum test
+                            statistic.
     """
 
     index = -1
     minEntropy = float("inf")
-    for i in range(0,self.m):
+    for i in xrange(self.m):
       entropy = 2 * self.W * stats.entropy(P_hat,self.P[i])
       if entropy < self.T and entropy < minEntropy:
         minEntropy = entropy
