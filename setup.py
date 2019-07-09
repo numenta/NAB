9c4fdf9779966e235981e95fd2d6b40916bc1307
Marek Otahal <markotahal@gmail.com>
2019-07-09 17:25:41 +0200

    typos

--- setup.py
+++ setup.py
@@ -28,8 +28,8 @@ REPO_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 # Utility function to read the README file.
-# Used for the long_description.  It"s nice, because now 1) we have a top level
-# README file and 2) it"s easier to type in the README file than to put a raw
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
   with open(os.path.join(os.path.dirname(__file__), fname)) as f:
